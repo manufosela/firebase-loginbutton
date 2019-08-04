cc75f42276dcad532969ffdf95d77cfa4f577a90
Manuel Juan Fosela Águila <manufosela@gmail.com>
2019-08-04 22:28:57 +0200

    fixed import path

--- firebase-loginbutton.js
+++ firebase-loginbutton.js
@@ -1,4 +1,4 @@
-import { LitElement, html, css } from '/node_modules/lit-element/lit-element.js';
+import { LitElement, html, css } from 'lit-element';
 import 'firebase/firebase-app';
 import 'firebase/firebase-auth';
 
