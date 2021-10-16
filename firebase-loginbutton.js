3586ee8d49770746834f8b3004ed4bf394c87967
Manuel Juan Fosela Águila <manufosela@gmail.com>
2021-10-16 12:47:34 +0200

    chore:updated lit and firebase

--- firebase-loginbutton.js
+++ firebase-loginbutton.js
@@ -1,4 +1,4 @@
-import { LitElement, html } from 'lit-element';
+import { LitElement, html } from 'lit';
 import { firebaseLoginbuttonStyles } from './firebase-loginbutton-style.js';
 
 import { initializeApp } from 'firebase/app';
